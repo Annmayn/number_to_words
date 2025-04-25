7a4d47805acfc2653662b2f642699527c36e9585
Neema Tsering <ntvirus333@gmail.com>
2025-04-25 21:00:27 +0545

    add nepali language

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,7 @@
+//source: https://github.com/mastermunj/to-words/tree/main/src/locales
 export const availableLocales = [
   ["French", "fr-FR"],
   ["Korean", "ko-KR"],
   ["English", "en-US"],
+  ["Nepali", "np-NP"],
 ];
